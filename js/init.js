9fe9c8635681c031bdadb92ff9592825b4fbe373
Josh Dean <josh.dean@procore.com>
2017-09-17 12:06:25 -0700

    fix mobile view for index

--- js/init.js
+++ js/init.js
@@ -1,4 +1,22 @@
 $(function(){
-  $('.header-wrapper').height(window.innerHeight);
-  $('.header-wrapper').css('margin-bottom', 0);
+  function init() {
+    $('.header-wrapper').height(window.innerHeight);
+    $('.header-wrapper').css('margin-bottom', 0);
+
+    resize();
+		window.addEventListener('resize', resize);
+  }
+
+  function resize() {
+		var _width = window.innerWidth;
+    if(_width <= 600) {
+      $('.index-card').height(window.innerHeight*0.8);
+      $('.index-card').width(window.innerWidth);
+    } else {
+      $('.index-card').css('height', '');
+      $('.index-card').css('width', '');
+    }
+	}
+
+  init();
 });
