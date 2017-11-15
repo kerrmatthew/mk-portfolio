01e081be455cac7468cb1b7789fed0452e8f2c49
Josh Dean <josh.dean@procore.com>
2017-11-14 19:40:11 -0800

    fix scroll on index

--- _site/js/init.js
+++ _site/js/init.js
@@ -4,15 +4,16 @@ $(document).ready(function(){
   $('.header-wrapper').css('margin-bottom', 0);
   $('.xoxo').fadeIn(500, 'linear');
 
-  function varyCardSpeed() {
-    function calculateMargin(card) {
-      var maxMargin = 200;
-      var offset = $(card).offset().top + $(card).height() / 2;
-      var pxFromMiddle = Math.abs(offset - window.innerHeight/2);
-      var ratioFromCenter = Math.max(0, 1 - pxFromMiddle / (window.innerHeight / 2));
-      return parseInt(maxMargin * ratioFromCenter);
-    }
 
+  function calculateMargin(card) {
+    var maxMargin = 200;
+    var offset = $(card).offset().top + $(card).height() / 2;
+    var pxFromMiddle = Math.abs(offset - window.innerHeight/2);
+    var ratioFromCenter = Math.max(0, 1 - pxFromMiddle / (window.innerHeight / 2));
+    return parseInt(maxMargin * ratioFromCenter);
+  }
+
+  if(window.innerWidth > 600){
     $('.xoxo').scroll(function() {
       $.each($('.index-card-wrapper'), function(i, card) {
         if(i % 2 == 0){
@@ -22,10 +23,8 @@ $(document).ready(function(){
         }
       });
     });
-  }
-
-  if(window.innerWidth > 600){
-    varyCardSpeed();
-    console.log('here we are');
+    console.log('varying card speed at '+window.innerWidth );
+  } else {
+    console.log('staying put at '+window.innerWidth );
   }
 });
