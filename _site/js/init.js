efad5cc7fa641901b500459c7a5d3c3d399bde6c
Matthew Kerr <matthew.kerr@me.com>
2017-11-19 13:44:46 +1300

    update some styles.

--- _site/js/init.js
+++ _site/js/init.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
-  $('.header-wrapper').css('min-height', window.innerHeight);
+  $('.header-wrapper').css('min-height' ,window.innerHeight);
   $('footer').css('min-height', window.innerHeight);
   $('.header-wrapper').css('margin-bottom', 0);
   $('.xoxo').fadeIn(500, 'linear');
 
+  function varyCardSpeed() {
+    function calculateMargin(card) {
+      var maxMargin = 200;
+      var offset = $(card).offset().top + $(card).height() / 2;
+      var pxFromMiddle = Math.abs(offset - window.innerHeight/2);
+      var ratioFromCenter = Math.max(0, 1 - pxFromMiddle / (window.innerHeight / 2));
+      return parseInt(maxMargin * ratioFromCenter);
+    }
 
-  function calculateMargin(card) {
-    var maxMargin = 200;
-    var offset = $(card).offset().top + $(card).height() / 2;
-    var pxFromMiddle = Math.abs(offset - window.innerHeight/2);
-    var ratioFromCenter = Math.max(0, 1 - pxFromMiddle / (window.innerHeight / 2));
-    return parseInt(maxMargin * ratioFromCenter);
-  }
-
-  if(window.innerWidth > 600){
     $('.xoxo').scroll(function() {
       $.each($('.index-card-wrapper'), function(i, card) {
         if(i % 2 == 0){
@@ -23,8 +22,10 @@ $(document).ready(function(){
         }
       });
     });
-    console.log('varying card speed at '+window.innerWidth );
-  } else {
-    console.log('staying put at '+window.innerWidth );
+  }
+
+  if(window.innerWidth > 600){
+    varyCardSpeed();
+    console.log('here we are');
   }
 });
