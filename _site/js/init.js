be1fc650d12b7b7bd74a20f10a57b4a6cbc622ca
Josh Dean <josh.dean@procore.com>
2017-11-19 16:05:01 -0800

    ping page looking good

--- _site/js/init.js
+++ _site/js/init.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
   $('footer').css('min-height', window.innerHeight);
   $('.header-wrapper').css('margin-bottom', 0);
   $('.xoxo').fadeIn(500, 'linear');
+
+  if(window.innerWidth < 600){
+    $('html').css({position:'fixed', overflow:'hidden'});
+    $('body').css({position:'fixed', overflow:'hidden'});
+  }
 });
